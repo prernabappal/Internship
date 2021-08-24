94bbc6437fd57eb62b2b73a60fe633102cb0920d
prernabappal <43715380+prernabappal@users.noreply.github.com>
2021-08-25 03:43:14 +0530

    Add files via upload

---
+++ Day 6/login.php
@@ -0,0 +1,9 @@
+<?php
+$name =$_POST["email"];
+echo "Email: ";
+echo $name;
+echo "<br>";
+$pass =$_POST["pass"];
+echo "Password: ";
+echo $pass;
+?>
---
+++ Day 6/register.php
@@ -0,0 +1,25 @@
+<?php
+$name =$_POST["name"];
+echo "Email: ";
+echo $name;
+echo"<br>";
+
+$contact =$_POST["contact"];
+echo "Contact: ";
+echo $contact;
+echo"<br>";
+
+$email =$_POST["email"];
+echo "Email: ";
+echo $email;
+echo"<br>";
+
+$addrs =$_POST["address"];
+echo "Address: ";
+echo $addrs;
+echo"<br>";
+
+$pass =$_POST["pass"];
+echo "Password: ";
+echo $pass;
+?>
